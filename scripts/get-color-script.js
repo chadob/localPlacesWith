730c7ebd412fde66da8b0c324e76ac499a0b7bb8
Chad Oehlschlaeger-Browne <chadnobrowne@gmail.com>
2018-05-11 13:26:49 -0700

    moving to laptop

--- scripts/get-color-script.js
+++ scripts/get-color-script.js
@@ -1,43 +1,38 @@
-
-function getColor(data, loc, numSections, category, sectionsObject) {
-  console.log(loc);
-  for(var i = numSections - 1; i > -1; i--) {
-    if(data[loc]["venueName"] === "Vidiot Arcade Bar") {
-      console.log(i);
-      console.log(sectionsObject[category][i]);
-      console.log(data[loc][sectionsObject[category][i]]);
-
+getColorObject {
+  getColor: function(data, loc, numSections, category, sectionsObject) {
+    console.log(loc);
+    for(var i = numSections - 1; i > -1; i--) {
+      if(data[loc]["venueName"] === "Vidiot Arcade Bar") {
+      }
+      if (data[loc][sectionsObject[category][i]]) {
+        return "" + i;
+      }
     }
-    if (data[loc][sectionsObject[category][i]]) {
-      return "" + i;
+  },
+
+  loopOverObject: function(object) {
+    var sectionsObject = {
+      "Bar Sports": ["pool", "pingPong", "darts", "cornhole", "foosball", "shuffleboard", "barSportsOther"],
+      "Live Entertainment": ["liveMusic", "karaoke", "dancing", "piano", "openMic", "comedy", "liveEntertainmentOther"],
+      "Games": ["skeeball", "jenga", "trivia", "boardGames", "videoGames", "arcades", "gamesOther"]
+    };
+    for (var loc in object) {
+      for (var category in sectionsObject) {
+        var numSections = sectionsObject[category].length;
+        var adjustedCat = category.replace(/([A-Z])/g, '$1').trim();
+        adjustedCat = adjustedCat.charAt(0).toLowerCase() + adjustedCat.substr(1);
+        adjustedCat = adjustedCat.replace(/\s+/g, '');
+        object[loc][adjustedCat + "Color"] = getColor(object, loc, numSections, category, sectionsObject);
+      }
     }
+    postData(object);
+  },
+
+  postData: function(data) {
+    console.log(data);
+    //posts to all data spreadsheet
+    var url = "https://script.google.com/macros/s/AKfycbx2S3un49m6cAUQML80HgTNGIT1_Mu-bmyhi_l0U788ED_jUdk/exec";  //
   }
-}
-
-
-function loopOverObject(object) {
-  var sectionsObject = {
-    "Bar Sports": ["pool", "pingPong", "darts", "cornhole", "foosball", "shuffleboard", "barSportsOther"],
-    "Live Entertainment": ["liveMusic", "karaoke", "dancing", "piano", "openMic", "comedy", "liveEntertainmentOther"],
-    "Games": ["skeeball", "jenga", "trivia", "boardGames", "videoGames", "arcades", "gamesOther"]
-  };
-  for (var loc in object) {
-    for (var category in sectionsObject) {
-      var numSections = sectionsObject[category].length;
-      console.log(numSections);
-      var adjustedCat = category.replace(/([A-Z])/g, '$1').trim();
-      adjustedCat = adjustedCat.charAt(0).toLowerCase() + adjustedCat.substr(1);
-      adjustedCat = adjustedCat.replace(/\s+/g, '');
-      object[loc][adjustedCat + "Color"] = getColor(object, loc, numSections, category, sectionsObject);
-    }
-  }
-  postData(object);
-}
-
-function postData(data) {
-  console.log(data);
-  //posts to all data spreadsheet
-  var url = "https://script.google.com/macros/s/AKfycbx2S3un49m6cAUQML80HgTNGIT1_Mu-bmyhi_l0U788ED_jUdk/exec";  //
 }
 //call the loop
 loopOverObject(gamesData);
--- scripts/get-coords-script.js
+++ scripts/get-coords-script.js
@@ -1,40 +1,40 @@
-
-function getCoords(data, loc, i, totalRuns) {
-  console.log('ran');
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode( { 'address': data[loc].streetAddress + ' ' + data[loc].city + ' ' + data[loc].state}, function(results, status) {
-    if (status == 'OK') {
-      data[loc].coords = results[0].geometry.viewport.f.f + ', ' + results[0].geometry.viewport.b.b;
-      if (i === totalRuns) {
-        console.log(data);
+getCoordsObject {
+  getCoords: function(data, loc, i, totalRuns) {
+    console.log('ran');
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': data[loc].streetAddress + ' ' + data[loc].city + ' ' + data[loc].state}, function(results, status) {
+      if (status == 'OK') {
+        data[loc].coords = results[0].geometry.viewport.f.f + ', ' + results[0].geometry.viewport.b.b;
+        if (i === totalRuns) {
+          console.log(data);
+        }
+        // return postData(jsonData);
+      } else {
+        console.log('Geocode was not successful for the following reason: ' + status);
       }
-      // return postData(jsonData);
-    } else {
-      console.log('Geocode was not successful for the following reason: ' + status);
+    });
+  },
+
+  doScaledTimeout: function(i, amount, object, prop, func, totalRuns) {
+    setTimeout(function() {
+      func(object, prop, i, totalRuns);
+    }, i * amount);
+  },
+
+  loopOverObject: function(amount, object, func) {
+    var totalRuns = Object.keys(allData).length;
+    var i = 0;
+    for (var loc in object) {
+      i++;
+      doScaledTimeout(i, amount, object, loc, func, totalRuns);
     }
-  });
-}
+  },
 
-function doScaledTimeout(i, amount, object, prop, func, totalRuns) {
-  setTimeout(function() {
-    func(object, prop, i, totalRuns);
-  }, i * amount);
-}
-
-function loopOverObject(amount, object, func) {
-  var totalRuns = Object.keys(allData).length;
-  var i = 0;
-  for (var loc in object) {
-    i++;
-    doScaledTimeout(i, amount, object, loc, func, totalRuns);
+  function postData(data) {
+    console.log(data);
+    //posts to all data spreadsheet
+    var url = "https://script.google.com/macros/s/AKfycbx2S3un49m6cAUQML80HgTNGIT1_Mu-bmyhi_l0U788ED_jUdk/exec";  //
   }
 }
 //call the loop
-loopOverObject(1000, allData, getCoords);
-
-function postData(data) {
-  console.log(data);
-  //posts to all data spreadsheet
-  var url = "https://script.google.com/macros/s/AKfycbx2S3un49m6cAUQML80HgTNGIT1_Mu-bmyhi_l0U788ED_jUdk/exec";  //
-
-}
+getCoordsObject.loopOverObject(1000, allData, getCoords);
